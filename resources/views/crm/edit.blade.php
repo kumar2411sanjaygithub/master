b671f35fca4c4ac163233ff25e3f9dd905389224
Sanjay <php6@cybuzzsc.com>
2018-09-21 09:44:47 +0530

    changes in login page

--- resources/views/crm/edit.blade.php
+++ resources/views/crm/edit.blade.php
@@ -186,10 +186,9 @@
 
 
                <div class="row">
-                  <div class="col-md-2">
+                  <div class="col-md-12">
                      <label  class="control-label"><u>ADDRESS INFORMATION</u></label>   <hr>
                   </div>
-                  <div class="col-md-9"></div>
                   <div class="col-md-1 " style="margin-left:-14px;"><button type="button" class="btn  btn-info btn-xs">LOCATE MAP</button></div>
                </div>
                <div class="row">
